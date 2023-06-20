7b36c8d411ef135bbe3accbed98f697474739069
s27451 <s27451@pjwstk.edu.pl>
2023-06-20 10:27:27 +0200

    Drobne zmiany estetyczne

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ if (isset($_SESSION["user_id"])) {
     $sql = "SELECT * FROM user WHERE id = {$_SESSION["user_id"]}";
     $result = $mysqli->query($sql);
     $user = $result->fetch_assoc();
-
     $_SESSION['name'] = $user['name'];
 }
 ?>
@@ -18,14 +17,16 @@ if (isset($_SESSION["user_id"])) {
     <title>TODO-APP</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <h1>TODO-APP</h1>
+    <h2>Prosta aplikacja typu TODO</h2>
 </head>
 <body>
 
-<h1>TODO-APP</h1>
+
 
 <?php if (isset($user)): ?>
 
-    <p>Witaj <?= htmlspecialchars($user["name"]) ?>!</p>
+    <h2>Witaj <?= htmlspecialchars($user["name"]) ?>!</h2>
     <p><a href="home.php">Przejdź do swoich zadań</a></p>
     <p><a href="projects.php">Przejdź do swoich projektów</a></p>
     <p><a href="calendar.php">Przejdź do swojego kalendarza</a></p>
